4b0731be03ef3dd7017f49a5ce4a4195bfb6c141
Šimon Steklík <simon.steklik@gmail.com>
2016-01-05 18:40:37 +0100

    implemented basic model methods

--- src/main/java/cz/fit/dpo/mvcshooter/model/Model.java
+++ src/main/java/cz/fit/dpo/mvcshooter/model/Model.java
@@ -1,5 +1,13 @@
 package cz.fit.dpo.mvcshooter.model;
 
+import cz.fit.dpo.mvcshooter.model.entities.Cannon;
+import cz.fit.dpo.mvcshooter.model.entities.Collision;
+import cz.fit.dpo.mvcshooter.model.entities.Enemy;
+import cz.fit.dpo.mvcshooter.model.entities.Missile;
+import cz.fit.dpo.mvcshooter.view.Canvas;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -8,9 +16,21 @@ import java.util.TimerTask;
  * @author Ondrej Stuchlik
  */
 public class Model {
-    
+
+    private List<Missile> missiles = new ArrayList<Missile>();
+
+    private List<Enemy> enemies = new ArrayList<Enemy>();
+
+    private List<Collision> collisions = new ArrayList<Collision>();
+
+    private Cannon cannon;
+
     private Timer timer;
-      
+
+    private int gravity;
+
+    private List<ModelObserver> observers = new ArrayList();
+
 
     public Model() {
         initTimer();
@@ -34,10 +54,59 @@ public class Model {
             public void run() {
                 // TODO implement
             }
-        }, 0, ModelConfig.TICK_TIME);
+    }, 0, ModelConfig.TICK_TIME);
     }
 
-    
+    public void moveCannonDown()
+    {
+        cannon.moveDown();
+    }
+
+    public void moveCannonUp()
+    {
+        cannon.moveUp();
+    }
+
+    public void forceOfCannonDown()
+    {
+        cannon.forceDown();
+    }
+
+    public void forceOfCannonUp()
+    {
+        cannon.forceUp();
+    }
+
+    public void aimCannonUp()
+    {
+        cannon.aimUp();
+    }
+
+    public void aimCannonDown()
+    {
+        cannon.aimDown();
+    }
+
+    public void shootCannon()
+    {
+        cannon.shoot();
+    }
+
+    public void increaseGravity()
+    {
+        gravity += ModelConfig.GRAVITY_STEP;
+    }
+
+    public void decreaseGravity()
+    {
+        gravity -= ModelConfig.GRAVITY_STEP;
+    }
+
+    public void registerObserver(ModelObserver observer)
+    {
+        observers.add(observer);
+    }
+
 
     // ################################## private logic ##################################
     
